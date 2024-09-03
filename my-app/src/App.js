ab4774785b04d57d10bc6f7ed8c83caa6207e221
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-03 20:31:34 +0000

    stateful class component and constructor learnt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import StatefulGreeting from "./components/StatefulGreeting";
 
 function App() {
   return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <Sidebar />
-    </div>
+    
+    <div className="App">
+      {/* Add your components here */ }
+      {/* <Sidebar /> */ }
+  <StatefulGreeting greeting="I'm learning Stateful class Component" name = "Mila" age ="18"/>
+    </div >
   );
 }
 
